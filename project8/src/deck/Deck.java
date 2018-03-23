84bd7538cbdfdf361ebe73f958b045dc99c33c1a
Chase Rivera <cmrivera353@gmail.com>
2018-03-23 16:18:16 -0500

    Add files via upload

---
+++ project8/src/deck/Deck.java
@@ -0,0 +1,77 @@
+package lab7;
+
+import java.util.Random;
+
+import lab7.Card.Suit;
+
+/**
+ * Class representing a standard 52-card deck of playing cards from which cards
+ * can be selected at random.
+ */
+public class Deck {
+	/**
+	 * The cards comprising this deck.
+	 */
+	private Card[] cards;
+
+	/**
+	 * The random number generator to use for selecting cards.
+	 */
+	private Random rand;
+
+	/**
+	 * Constructs a new deck with a default random number generator.
+	 */
+	public Deck() {
+		rand = new Random();
+		init();
+	}
+
+	/**
+	 * Constructs a new deck with the given random number generator.
+	 */
+	public Deck(Random givenGenerator) {
+		rand = givenGenerator;
+		init();
+	}
+
+	/**
+	 * Returns a new array containing k elements selected at random from this
+	 * deck.
+	 */
+	public Card[] select(int k) {
+		Card[] results = new Card[k];
+		
+		for(int i = 0; i < k; i++){
+			int pos = rand.nextInt(cards.length - (i + 1));
+			Card temp = cards[cards.length - (i + 1)];
+			cards[cards.length - (i + 1)] = cards[pos];
+			cards[pos] = temp;
+		}
+		
+		for(int i = 0; i < k; i++){
+			results[i] = cards[cards.length - (i + 1)];
+		}
+		
+		return results;
+	}
+
+	/**
+	 * Initializes a new deck of 52 cards.
+	 */
+	private void init() {
+		cards = new Card[52];
+		int index = 0;
+		for (int rank = 1; rank <= 13; ++rank) {
+			cards[index] = new Card(rank, Suit.CLUBS);
+			index += 1;
+			cards[index] = new Card(rank, Suit.DIAMONDS);
+			index += 1;
+			cards[index] = new Card(rank, Suit.HEARTS);
+			index += 1;
+			cards[index] = new Card(rank, Suit.SPADES);
+			index += 1;
+		}
+
+	}
+}
